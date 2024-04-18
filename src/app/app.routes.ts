55ccbf88c4d075d767afd0229958c7092a5f7160
Diana Gil <dianamarcela.gilburitica@ciepp.com>
2024-04-18 10:51:49 +0200

    Se agregaron componentes nuevos y rutas de acceso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'home', title:'Home', component:HomeComponent},
+    {path:'navbar', title:'Navbar', component:NavbarComponent},
+    {path:'footer', title:'Footer', component:FooterComponent},
+    {path:'', redirectTo:'/home', pathMatch:'full'},
+    {path:'**', title:'PAGE NOT FOUND', component:PageNotFoundComponent}
+
+
+
+
+
+];
