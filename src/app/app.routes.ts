d2bfe0abb05175ee388e36fa28773042bd1cde3d
BerthaGr <BerthaGr@gmail.com>
2024-04-18 10:51:19 +0200

    se agregaron componentes nuevos y routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'Home',
+        component: HomeComponent
+    },
+    {
+        path: 'navbar',
+        title: 'Navbar',
+        component: NavbarComponent
+    },
+    {
+        path: 'footer',
+        title: 'Footer',
+        component: FooterComponent
+    }
+];
