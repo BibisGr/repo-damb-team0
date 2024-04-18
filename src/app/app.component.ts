bd474f13be846c5fc3acb0ceb1f4f8cbe3d29290
Esteban <e.castanyerpardo@gmail.com>
2024-04-18 10:53:38 +0200

    se agregaron componentes nuevos y routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent,FooterComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        title: 'Home',
+        component: HomeComponent
+        
+    },
+    {   
+        path: 'navbar',
+        title: 'Navbar',
+        component: NavbarComponent
+    },
+    {
+        path: 'footer',
+        title: 'Footer',
+        component: FooterComponent
+    }
+];
